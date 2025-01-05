377f28e71b6faccfb70ae60430f15728b7f650b0
S-kaif123 <kaifbhombal.kb@gmail.com>
2025-01-05 10:57:35 +0530

    added changes

---
+++ src/main/java/net/javaguides/ems/repository/EmployeeRepository.java
@@ -0,0 +1,5 @@
+package net.javaguides.ems.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import  net.javaguides.ems.entity.Employee;
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package net.javaguides.ems.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.ems.dto.EmployeeDto;
+import net.javaguides.ems.exception.ResourceNotFoundException;
+import net.javaguides.ems.mapper.EmployeeMapper;
+import net.javaguides.ems.repository.EmployeeRepository;
+import net.javaguides.ems.service.EmployeeService;
+import net.javaguides.ems.entity.Employee;
+import net.javaguides.ems.mapper.EmployeeMapper;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    
+
+}
